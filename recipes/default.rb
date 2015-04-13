e8fe7ee6b4a9dce1010e4ff597639f84f7495948
Erez Rabih <erez.rabih@gmail.com>
2015-04-13 14:17:44 +0300

    run the apt recipe before everythign

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'apt'
+
 apt_repository 'mongodb' do
   uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
   distribution 'dist'
